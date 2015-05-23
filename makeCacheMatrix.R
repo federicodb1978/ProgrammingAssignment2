0ba16baddf64f1ad548ceedf511ce87f9322ae2d
fededb <fededellabella@yahoo.it>
2015-05-23 11:51:06 +0200

    secondo commit con piccole variazioni

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,11 +1,11 @@
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL # sets the value of m to NULL (provides a default if cacheSolve has not yet been used)
-        y <- NULL # sets the value of y to NULL (provides a default if cacheSolve has not yet been used)
+        m <- NULL # sets the value of m to NULL (default value)
+        y <- NULL # sets the value of y to NULL (default value)
         setmatrix <- function(y) { #set the value of the matrix
-                x <<- y ## caches the inputted matrix so that cacheSolve can check whether it has changed (note this is within the setmatrix function)
-                m <<- NULL # # sets the value of m (the matrix inverse if used cacheSolve) to NULL
+                x <<- y ## save in cache the inputted matrix: cacheSolve can check whether the value in cache has been changed
+                m <<- NULL # # sets the value of m to NULL; m is the inverse matrix in case you use cacheSolve
         }
-        list(setmatrix = setmatrix, getmatrix = getmatrix, # creates a list to house the four functions
+        list(setmatrix = setmatrix, getmatrix = getmatrix, # the list to house the four functions
              setinverse = setinverse,
              getinverse = getinverse)
 }
